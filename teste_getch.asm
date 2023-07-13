285327faf55ba5c8b83003badcfb49ac7130ac67
MelBWG <giulia.giannetti@gmail.com>
2023-07-12 21:42:24 -0300

    corrige programa de teste

--- teste_getch.asm
+++ teste_getch.asm
@@ -3,31 +3,32 @@ section .data
 resposta    db  'Voce digitou '
 s_resposta  dw  $-resposta
 nwln        db  0dh,0ah
-contador    db  4
+contador    db  3
 
 section .text
 _main:  sub esp,4
 loop:   mov eax,3
         mov ebx,0
         mov ecx,esp
+        add ecx,contador
         mov edx,1
-        int 0x80
-        mov eax,4
-        mov ebx,1
-        mov ecx, resposta
-        mov edx,s_resposta
-        int 0x80
-        mov ecx,esp
-        mov edx,1
-        int 0x80
-        mov ecx,nwln
-        mov edx,2
         int 0x80
         mov eax,[contador]
         dec eax
         mov [contador],eax
         cmp contador,0
         jne loop
+        mov eax,4
+        mov ebx,1
+        mov ecx, resposta
+        mov edx,s_resposta
+        int 0x80
+        mov ecx,esp
+        mov edx,4
+        int 0x80
+        mov ecx,nwln
+        mov edx,2
+        int 0x80
         mov eax 0
         mov ebx 1
         int 0x80
